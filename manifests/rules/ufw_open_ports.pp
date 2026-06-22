1cf2f52eff241d40ad35fb78ed175986bb8235d3
Marco Roque Andrade <mroqueandrade@aurora.tech>
2026-06-22 11:56:00 -0500

    Allow ufw_open_ports and ufw_outbound to accpet integers and port ranges

--- manifests/rules/ufw_open_ports.pp
+++ manifests/rules/ufw_open_ports.pp
@@ -43,10 +43,10 @@ class cis_security_hardening::rules::ufw_open_ports (
       }
 
       if cis_security_hardening::hash_key($data, 'port') {
-        unless $data['port'] =~ /^\d+$/ {
+        unless $data['port'] =~ /^\d+(:\d+)?$/ {
           fail("Illegal port: ${data['port']}")
         }
-        $port = $data['port']
+        $port = String($data['port'])
       } else {
         $port = ''
       }
